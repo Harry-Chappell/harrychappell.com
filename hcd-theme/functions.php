b7f67855c00c8e91177968fdabaa500b5f3422d3
Harry Chappell <harry@hcdigital.co.uk>
2022-07-30 17:22:03 +0100

    Adding HCD Theme

---
+++ hcd-theme/functions.php
@@ -0,0 +1,408 @@
+<?php
+/*
+Author: Eddie Machado
+URL: http://themble.com/bones/
+
+This is where you can drop your custom functions or
+just edit things like thumbnail sizes, header images,
+sidebars, comments, etc.
+*/
+
+// LOAD BONES CORE (if you remove this, the theme will break)
+require_once( 'library/bones.php' );
+
+// CUSTOMIZE THE WORDPRESS ADMIN (off by default)
+// require_once( 'library/admin.php' );
+
+/*********************
+LAUNCH BONES
+Let's get everything up and running.
+*********************/
+
+function bones_ahoy() {
+
+  //Allow editor style.
+  add_editor_style( get_stylesheet_directory_uri() . '/library/css/editor-style.css' );
+
+  // let's get language support going, if you need it
+  load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
+
+  // USE THIS TEMPLATE TO CREATE CUSTOM POST TYPES EASILY
+  require_once( 'library/custom-post-type.php' );
+
+  // launching operation cleanup
+  add_action( 'init', 'bones_head_cleanup' );
+  // A better title
+  add_filter( 'wp_title', 'rw_title', 10, 3 );
+  // remove WP version from RSS
+  add_filter( 'the_generator', 'bones_rss_version' );
+  // remove pesky injected css for recent comments widget
+  add_filter( 'wp_head', 'bones_remove_wp_widget_recent_comments_style', 1 );
+  // clean up comment styles in the head
+  add_action( 'wp_head', 'bones_remove_recent_comments_style', 1 );
+  // clean up gallery output in wp
+  add_filter( 'gallery_style', 'bones_gallery_style' );
+
+  // enqueue base scripts and styles
+  add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
+  // ie conditional wrapper
+
+  // launching this stuff after theme setup
+  bones_theme_support();
+
+  // adding sidebars to Wordpress (these are created in functions.php)
+  add_action( 'widgets_init', 'bones_register_sidebars' );
+
+  // cleaning up random code around images
+  add_filter( 'the_content', 'bones_filter_ptags_on_images' );
+  // cleaning up excerpt
+  add_filter( 'excerpt_more', 'bones_excerpt_more' );
+
+} /* end bones ahoy */
+
+// let's get this party started
+add_action( 'after_setup_theme', 'bones_ahoy' );
+
+
+/************* OEMBED SIZE OPTIONS *************/
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 680;
+}
+
+/************* THUMBNAIL SIZE OPTIONS *************/
+
+// Thumbnail sizes
+add_image_size( 'bones-thumb-600', 600, 150, true );
+add_image_size( 'bones-thumb-300', 300, 100, true );
+
+/*
+to add more sizes, simply copy a line from above
+and change the dimensions & name. As long as you
+upload a "featured image" as large as the biggest
+set width or height, all the other sizes will be
+auto-cropped.
+
+To call a different size, simply change the text
+inside the thumbnail function.
+
+For example, to call the 300 x 100 sized image,
+we would use the function:
+<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
+for the 600 x 150 image:
+<?php the_post_thumbnail( 'bones-thumb-600' ); ?>
+
+You can change the names and dimensions to whatever
+you like. Enjoy!
+*/
+
+add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
+
+function bones_custom_image_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'bones-thumb-600' => __('600px by 150px'),
+        'bones-thumb-300' => __('300px by 100px'),
+    ) );
+}
+
+/*
+The function above adds the ability to use the dropdown menu to select
+the new images sizes you have just created from within the media manager
+when you add media to your content blocks. If you add more image sizes,
+duplicate one of the lines in the array and name it according to your
+new image size.
+*/
+
+/************* THEME CUSTOMIZE *********************/
+
+/* 
+  A good tutorial for creating your own Sections, Controls and Settings:
+  http://code.tutsplus.com/series/a-guide-to-the-wordpress-theme-customizer--wp-33722
+  
+  Good articles on modifying the default options:
+  http://natko.com/changing-default-wordpress-theme-customization-api-sections/
+  http://code.tutsplus.com/tutorials/digging-into-the-theme-customizer-components--wp-27162
+  
+  To do:
+  - Create a js for the postmessage transport method
+  - Create some sanitize functions to sanitize inputs
+  - Create some boilerplate Sections, Controls and Settings
+*/
+
+function bones_theme_customizer($wp_customize) {
+  // $wp_customize calls go here.
+  //
+  // Uncomment the below lines to remove the default customize sections 
+
+  // $wp_customize->remove_section('title_tagline');
+  // $wp_customize->remove_section('colors');
+  // $wp_customize->remove_section('background_image');
+  // $wp_customize->remove_section('static_front_page');
+  // $wp_customize->remove_section('nav');
+
+  // Uncomment the below lines to remove the default controls
+  // $wp_customize->remove_control('blogdescription');
+  
+  // Uncomment the following to change the default section titles
+  // $wp_customize->get_section('colors')->title = __( 'Theme Colors' );
+  // $wp_customize->get_section('background_image')->title = __( 'Images' );
+}
+
+add_action( 'customize_register', 'bones_theme_customizer' );
+
+/************* ACTIVE SIDEBARS ********************/
+
+// Sidebars & Widgetizes Areas
+function bones_register_sidebars() {
+	register_sidebar(array(
+		'id' => 'sidebar1',
+		'name' => __( 'Sidebar 1', 'bonestheme' ),
+		'description' => __( 'The first (primary) sidebar.', 'bonestheme' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+	/*
+	to add more sidebars or widgetized areas, just copy
+	and edit the above sidebar code. In order to call
+	your new sidebar just use the following code:
+
+	Just change the name to whatever your new
+	sidebar's id is, for example:
+
+	register_sidebar(array(
+		'id' => 'sidebar2',
+		'name' => __( 'Sidebar 2', 'bonestheme' ),
+		'description' => __( 'The second (secondary) sidebar.', 'bonestheme' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+	To call the sidebar in your template, you can just copy
+	the sidebar.php file and rename it to your sidebar's name.
+	So using the above example, it would be:
+	sidebar-sidebar2.php
+
+	*/
+} // don't remove this bracket!
+
+
+/************* COMMENT LAYOUT *********************/
+
+// Comment Layout
+function bones_comments( $comment, $args, $depth ) {
+   $GLOBALS['comment'] = $comment; ?>
+  <div id="comment-<?php comment_ID(); ?>" <?php comment_class('cf'); ?>>
+    <article  class="cf">
+      <header class="comment-author vcard">
+        <?php
+        /*
+          this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
+          echo get_avatar($comment,$size='32',$default='<path_to_url>' );
+        */
+        ?>
+        <?php // custom gravatar call ?>
+        <?php
+          // create variable
+          $bgauthemail = get_comment_author_email();
+        ?>
+        <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
+        <?php // end custom gravatar call ?>
+        <?php printf(__( '<cite class="fn">%1$s</cite> %2$s', 'bonestheme' ), get_comment_author_link(), edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ) ?>
+        <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
+
+      </header>
+      <?php if ($comment->comment_approved == '0') : ?>
+        <div class="alert alert-info">
+          <p><?php _e( 'Your comment is awaiting moderation.', 'bonestheme' ) ?></p>
+        </div>
+      <?php endif; ?>
+      <section class="comment_content cf">
+        <?php comment_text() ?>
+      </section>
+      <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+    </article>
+  <?php // </li> is added by WordPress automatically ?>
+<?php
+} // don't remove this bracket!
+
+
+/*
+This is a modification of a function found in the
+twentythirteen theme where we can declare some
+external fonts. If you're using Google Fonts, you
+can replace these fonts, change it in your scss files
+and be up and running in seconds.
+*/
+function bones_fonts() {
+  wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+}
+
+add_action('wp_enqueue_scripts', 'bones_fonts');
+
+
+
+/*
+ * Function for post duplication. Dups appear as drafts. User is redirected to the edit screen
+ */
+function rd_duplicate_post_as_draft(){
+	global $wpdb;
+	if (! ( isset( $_GET['post']) || isset( $_POST['post'])  || ( isset($_REQUEST['action']) && 'rd_duplicate_post_as_draft' == $_REQUEST['action'] ) ) ) {
+		wp_die('No post to duplicate has been supplied!');
+	}
+ 
+	/*
+	 * Nonce verification
+	 */
+	if ( !isset( $_GET['duplicate_nonce'] ) || !wp_verify_nonce( $_GET['duplicate_nonce'], basename( __FILE__ ) ) )
+		return;
+ 
+	/*
+	 * get the original post id
+	 */
+	$post_id = (isset($_GET['post']) ? absint( $_GET['post'] ) : absint( $_POST['post'] ) );
+	/*
+	 * and all the original post data then
+	 */
+	$post = get_post( $post_id );
+ 
+	/*
+	 * if you don't want current user to be the new post author,
+	 * then change next couple of lines to this: $new_post_author = $post->post_author;
+	 */
+	$current_user = wp_get_current_user();
+	$new_post_author = $current_user->ID;
+ 
+	/*
+	 * if post data exists, create the post duplicate
+	 */
+	if (isset( $post ) && $post != null) {
+ 
+		/*
+		 * new post data array
+		 */
+		$args = array(
+			'comment_status' => $post->comment_status,
+			'ping_status'    => $post->ping_status,
+			'post_author'    => $new_post_author,
+			'post_content'   => $post->post_content,
+			'post_excerpt'   => $post->post_excerpt,
+			'post_name'      => $post->post_name,
+			'post_parent'    => $post->post_parent,
+			'post_password'  => $post->post_password,
+			'post_status'    => 'draft',
+			'post_title'     => $post->post_title,
+			'post_type'      => $post->post_type,
+			'to_ping'        => $post->to_ping,
+			'menu_order'     => $post->menu_order
+		);
+ 
+		/*
+		 * insert the post by wp_insert_post() function
+		 */
+		$new_post_id = wp_insert_post( $args );
+ 
+		/*
+		 * get all current post terms ad set them to the new post draft
+		 */
+		$taxonomies = get_object_taxonomies($post->post_type); // returns array of taxonomy names for post type, ex array("category", "post_tag");
+		foreach ($taxonomies as $taxonomy) {
+			$post_terms = wp_get_object_terms($post_id, $taxonomy, array('fields' => 'slugs'));
+			wp_set_object_terms($new_post_id, $post_terms, $taxonomy, false);
+		}
+ 
+		/*
+		 * duplicate all post meta just in two SQL queries
+		 */
+		$post_meta_infos = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id");
+		if (count($post_meta_infos)!=0) {
+			$sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
+			foreach ($post_meta_infos as $meta_info) {
+				$meta_key = $meta_info->meta_key;
+				if( $meta_key == '_wp_old_slug' ) continue;
+				$meta_value = addslashes($meta_info->meta_value);
+				$sql_query_sel[]= "SELECT $new_post_id, '$meta_key', '$meta_value'";
+			}
+			$sql_query.= implode(" UNION ALL ", $sql_query_sel);
+			$wpdb->query($sql_query);
+		}
+ 
+ 
+		/*
+		 * finally, redirect to the edit post screen for the new draft
+		 */
+		wp_redirect( admin_url( 'post.php?action=edit&post=' . $new_post_id ) );
+		exit;
+	} else {
+		wp_die('Post creation failed, could not find original post: ' . $post_id);
+	}
+}
+add_action( 'admin_action_rd_duplicate_post_as_draft', 'rd_duplicate_post_as_draft' );
+ 
+/*
+ * Add the duplicate link to action list for post_row_actions
+ */
+function rd_duplicate_post_link( $actions, $post ) {
+	if (current_user_can('edit_posts')) {
+		$actions['duplicate'] = '<a href="' . wp_nonce_url('admin.php?action=rd_duplicate_post_as_draft&post=' . $post->ID, basename(__FILE__), 'duplicate_nonce' ) . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
+	}
+	return $actions;
+}
+ 
+add_filter( 'post_row_actions', 'rd_duplicate_post_link', 10, 2 );
+
+
+
+
+//Custom Menus
+function wpb_custom_new_menu() {
+  register_nav_menus(
+    array(
+      'mobile-menu' => __( 'Mobile Menu' ),
+    )
+  );
+}
+add_action( 'init', 'wpb_custom_new_menu' );
+
+
+
+
+//New Widget Area
+if ( function_exists('register_sidebar') )
+  register_sidebar(array(
+    'name' => 'Widget Area Name',
+    'before_widget' => '<div class = "widgetizedArea">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3>',
+    'after_title' => '</h3>',
+  )
+);
+
+
+
+
+// Add browser class to body
+add_filter('body_class','browser_body_class');
+function browser_body_class($classes) {
+global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+if($is_lynx) $classes[] = 'lynx';
+elseif($is_gecko) $classes[] = 'gecko';
+elseif($is_opera) $classes[] = 'opera';
+elseif($is_NS4) $classes[] = 'ns4';
+elseif($is_safari) $classes[] = 'safari';
+elseif($is_chrome) $classes[] = 'chrome';
+elseif($is_IE) $classes[] = 'ie';
+else $classes[] = 'unknown';
+if($is_iphone) $classes[] = 'iphone';
+return $classes;
+}
+
+
+
+
+
+/* DON'T DELETE THIS CLOSING TAG */ ?>
