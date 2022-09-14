0cf30db60aaeda0184a53c8781e8ae743a7bcc7f
Harry Chappell <harry@hcdigital.co.uk>
2022-09-14 15:32:22 +0100

    Slider functionality

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     
     </head>
     
-    <body <?php body_class(); ?>>
+    <body <?php body_class( "detail1" ); ?>>
         <?php wp_body_open(); ?>
 
         <button class="btn-toggle">Toggle Dark-Mode</button>
@@ -48,3 +48,30 @@
 
             <div id="container">
                 <main id="content" role="main">
+
+                <input id="detail-slider" type="range" min="1" max="7" value="1" list="tickmarks">
+                <output></output>
+                <datalist id="tickmarks">
+                <option value="1"></option>
+                <option value="2"></option>
+                <option value="3"></option>
+                <option value="4"></option>
+                <option value="5"></option>
+                <option value="6"></option>
+                <option value="7"></option>
+                </datalist>
+
+                <script>
+                    const slider = document.querySelector("#detail-slider");
+
+                    slider.addEventListener("input", function () {
+                        document.body.classList.remove("detail1");
+                        document.body.classList.remove("detail2");
+                        document.body.classList.remove("detail3");
+                        document.body.classList.remove("detail4");
+                        document.body.classList.remove("detail5");
+                        document.body.classList.remove("detail6");
+                        document.body.classList.remove("detail7");
+                        document.body.classList.add("detail" + slider.value);
+                    });
+                </script>
