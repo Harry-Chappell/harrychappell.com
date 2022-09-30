f45d5fb03c4aeace6139cef50a5bc24a7df1a7ed
Harry Chappell <harry@hcdigital.co.uk>
2022-09-30 12:46:49 +0100

    Slider styling

--- header.php
+++ header.php
@@ -41,29 +41,32 @@
             <div id="container">
                 <main id="content" role="main">
 
-                <input id="detail-slider" type="range" min="1" max="7" value="5" list="tickmarks">
-                <output></output>
-                <datalist id="tickmarks">
-                <option value="1"></option>
-                <option value="2"></option>
-                <option value="3"></option>
-                <option value="4"></option>
-                <option value="5"></option>
-                <option value="6"></option>
-                <option value="7"></option>
-                </datalist>
+                <div class="slider-cntr">
+                    <input id="detail-slider" type="range" min="1" max="7" value="5" list="tickmarks">
+                    <output></output>
+                    <datalist id="tickmarks">
+                    <option value="1"></option>
+                    <option value="2"></option>
+                    <option value="3"></option>
+                    <option value="4"></option>
+                    <option value="5"></option>
+                    <option value="6"></option>
+                    <option value="7"></option>
+                    </datalist>
 
-                <script>
-                    const slider = document.querySelector("#detail-slider");
+                    <script>
+                        const slider = document.querySelector("#detail-slider");
 
-                    slider.addEventListener("input", function () {
-                        document.body.classList.remove("detail1");
-                        document.body.classList.remove("detail2");
-                        document.body.classList.remove("detail3");
-                        document.body.classList.remove("detail4");
-                        document.body.classList.remove("detail5");
-                        document.body.classList.remove("detail6");
-                        document.body.classList.remove("detail7");
-                        document.body.classList.add("detail" + slider.value);
-                    });
-                </script>
+                        slider.addEventListener("input", function () {
+                            document.body.classList.remove("detail1");
+                            document.body.classList.remove("detail2");
+                            document.body.classList.remove("detail3");
+                            document.body.classList.remove("detail4");
+                            document.body.classList.remove("detail5");
+                            document.body.classList.remove("detail6");
+                            document.body.classList.remove("detail7");
+                            document.body.classList.add("detail" + slider.value);
+                        });
+                    </script>
+
+                </div>
